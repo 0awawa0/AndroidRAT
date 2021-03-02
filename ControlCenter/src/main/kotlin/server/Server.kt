f3cc6417ed14d42f2ba9dafd292305594712bd86
Alexander Pozdniakov <mail.0awawa0@gmail.com>
2021-03-02 21:04:41 +0300

    Fixed exchanging data between server and client

--- ControlCenter/src/main/kotlin/server/Server.kt
+++ ControlCenter/src/main/kotlin/server/Server.kt
@@ -9,6 +9,8 @@ import io.ktor.network.sockets.*
 import io.ktor.utils.io.*
 import kotlinx.coroutines.*
 import java.net.*
+import java.net.ServerSocket
+import java.net.Socket
 import java.nio.ByteBuffer
 import java.nio.channels.ServerSocketChannel
 import java.nio.channels.SocketChannel
@@ -29,94 +31,34 @@ class ServerThread: Thread() {
     private val dataDivider = ":|:"
 
     private val threadPool = Executors.newCachedThreadPool()
-    private val selector = ActorSelectorManager(threadPool.asCoroutineDispatcher())
-    private val tcpSocketBuilder = aSocket(selector).tcp()
-    private val server = tcpSocketBuilder.bind("127.0.0.1", 33455)
+
+    private val socket = ServerSocket(33455)
 
     private var clients = HashMap<String, Client>()
 
     override fun run() {
-        runBlocking {
-            running = true
 
-            Logger.log(tag, "Server started")
-            while (running) {
-                val client = server.accept()
+        running = true
+        while (running) {
+            val client = socket.accept()
 
-                launch {
-                    val input = client.openReadChannel()
-                    val output = client.openWriteChannel(true)
-                    try {
-                        while (true) {
-                            val line = input.readUTF8Line()
-                            if (line?.isNotBlank() == true) {
-                                val lineArray = line.split(dataDivider)
+            threadPool.submit {
+                Logger.log(tag, "Client connected: ${client.remoteSocketAddress}")
+                while (client.isConnected) {
+                    val output = client.getOutputStream()
+                    val input = client.getInputStream().bufferedReader()
 
-                                when (val command = lineArray[0].trim()) {
-                                    "connect" -> {
-                                        val token = lineArray.getOrElse(1) { "" }
-
-                                        if (token.isBlank()) {
-                                            output.writeStringUtf8("error${dataDivider}token required\n")
-                                            continue
-                                        }
-
-                                        val uuid = if (token in clients.values) {
-                                            clients.let {
-                                                clients.forEach {
-                                                    if (it.value.token == token) return@let it.key
-                                                }
-                                                var ret = UUID.randomUUID().toString()
-                                                while (ret in clients.keys) ret = UUID.randomUUID().toString()
-                                                return@let ret
-                                            }
-                                        } else {
-                                            var ret = UUID.randomUUID().toString()
-                                            while (ret in clients.keys) ret = UUID.randomUUID().toString()
-                                            ret
-                                        }
-
-                                        clients[uuid] = Client(uuid, token)
-                                        Logger.log(
-                                            tag,
-                                            "Device requested connect. Token received: $token. UUID dedicated: $uuid."
-                                        )
-
-                                        output.writeStringUtf8("ok$dataDivider$uuid\n")
-
-                                        val clientList = clients.values.toList()
-                                        listeners.forEach { it.onClientsListChanged(clientList) }
-                                    }
-
-                                    "contacts" -> {
-                                        val uuid = lineArray.getOrElse(1) { "" }
-                                        val contacts = lineArray.getOrElse(2) { "" }
-
-                                        if (uuid.isBlank()) {
-                                            output.writeStringUtf8("error${dataDivider}UUID required\n")
-                                            continue
-                                        }
-
-                                        Logger.log(tag, "Received contacts from $uuid\nContacts: $contacts")
-
-                                        output.writeStringUtf8("ok\n")
-                                    }
-
-                                    else -> { Logger.log(tag, "Received command: $command")}
-                                }
-                            } else {
-                                delay(50)
-                            }
-                        }
-                    } catch (ex: Throwable) {
-                        ex.printStackTrace()
-                        client.close()
+                    while (true) {
+                        val line = input.readLine()
+                        Logger.log(tag, "Received $line")
+                        output.write("line\n".toByteArray())
+                        Logger.log(tag, "Sent back")
                     }
                 }
+                Logger.log(tag, "Client disconnected: ${client.remoteSocketAddress}")
             }
-            Logger.log(tag, "Server stopped")
 
-            server.close()
+            sleep(10)
         }
     }
 
